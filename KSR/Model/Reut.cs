0aeafafac08e8fa1805b2ea51e54ec6634329677
Lukasz Rogozinski <gglukasz@gmail.com>
2018-04-13 17:06:54 +0200

    Wczytywanie plikow sgm

---
+++ KSR/Model/Reut.cs
@@ -0,0 +1,46 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSR.Model
+{
+    public class Reut
+    {
+        public List<string> Places { get; set; }
+        public string Text { get; set; }
+
+        public static List<Reut> GetReutersFromFile(string[] reutPath)
+        {
+            List<Reut> reuters = new List<Reut>();
+            int reutersNumber = reutPath.Length;
+            for (int i = 0; i < reutersNumber; i++)
+            {
+                var xmlRawFile = File.ReadAllText(reutPath[i]);
+                var html = new HtmlDocument();
+                html.LoadHtml(xmlRawFile);
+                reuters.AddRange(html.DocumentNode.Descendants("REUTERS").Select(x => new Reut
+                {
+                    Places = x.Descendants("PLACES").Select(c => c.Descendants("D").Select(h => h.InnerHtml)).First().ToList(),
+                    Text = x.Descendants("TEXT").Select(
+                            z => z.LastChild.InnerHtml
+                            .Substring(0, z.LastChild.InnerHtml.Length >= 13 ? z.LastChild.InnerHtml.Length - 13 : 0)
+                        ).First()
+                }).ToList());
+            }
+            int howManyReuters = reuters.Count;
+            for (int i=0; i<reuters.Count; i++)
+            {
+                if (reuters.ElementAt(i).Places.Count != 1)
+                {
+                    reuters.Remove(reuters.ElementAt(i));
+                }
+            }
+
+            return reuters;
+        }
+    }
+}
---
+++ KSR/ViewModel/MyViewModel.cs
@@ -0,0 +1,58 @@
+﻿using KSR.Model;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace KSR.ViewModel
+{
+    public class MyViewModel : INotifyPropertyChanged
+    {
+        #region Constructors
+        public MyViewModel()
+        {
+            Load_Files = new DelegateCommand(LoadReuters);
+        }
+        #endregion
+
+        #region ICommand
+        public ICommand Load_Files { get; }
+        #endregion
+
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string propertyName_)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName_));
+        }
+        #endregion
+
+        #region Private
+        private List<Reut> reuters;
+        #endregion
+
+        #region Properties
+        public List<Reut> Reuters
+        {
+            get { return reuters; }
+            set { this.reuters = value; }
+        }
+        #endregion
+
+        #region Functions
+        private void LoadReuters()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Sgm File(*.sgm)| *.sgm";
+            openFileDialog.Multiselect = true;
+            openFileDialog.ShowDialog();
+            string[] path = openFileDialog.FileNames;
+            Reuters = Reut.GetReutersFromFile(path);
+        }
+        #endregion
+    }
+}
